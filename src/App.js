a8b4cb1b81de4621a135794244f70866b5ade6ef
joalvarado2 <jonathanalva92@gmail.com>
2021-03-29 22:17:15 -0500

    integrando componentes y API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./Components/Header";
+import Formulario from "./Components/Formulario";
+import Clima from "./Components/Clima";
 
 function App() {
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const { ciudad, pais } = busqueda;
+
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if (consultar) {
+        const appId = "4aaf04af1acd01876a5337f9b4b1b81b";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setResultado({resultado})
+        console.log(resultado)
+        setConsultar(false);
+      }
+    };
+    consultarApi();
+  }, [consultar]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header titulo="Clima React App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+            <Clima
+              resultado={resultado}
+            />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
